db91f9bb6dcff2f5b4d103c74b3079c7820bbd5b
Ricardo Nunes <53958168+Ricknsx@users.noreply.github.com>
2020-08-11 15:53:21 -0300

    Rodapé
    Rodapé adicionado, com mudanças no CSS e Header

--- oldFriend/view/header.php
+++ oldFriend/view/header.php
@@ -6,8 +6,12 @@
     
     <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-with-button-logo.css">
     <link type="text/css" rel="stylesheet" href="../css/style.css"  />
     <link type="text/css" rel="stylesheet" href="css/style.css"  />
+    
     <title>Old Friend</title>
 </head>
 <body>
