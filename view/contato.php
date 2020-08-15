8949f0e08e800ce71f1d310a5978f331ea3a13fb
danilocucharro <53611163+danilocucharro@users.noreply.github.com>
2020-08-15 12:28:08 -0300

    alteracoes do front-end pagina contato.php

--- view/contato.php
+++ view/contato.php
@@ -11,35 +11,24 @@
     
     <header> <?php include("header.php")?> </header>
     <section class ="corpo-FAQ">
-        <div class="titulo-faq">
-            <h1><strong>Duvidas Frequentes</strong></h1>
-        </div>
         
         <div class="duvidas">
-            <button id="duvida1"><h4>É pago</h4></button><br>
-            <button><h4>A comunicacao e confiavel</h4></button><br>
-            <button><h4>Como funciona o processo</h4></button><br>
-            <button><h4>Quais são os benefícios</h4></button><br>
-            <button><h4>Dificuldades em achar oque procura</h4></button>
+        <figcaption>
+				<h2> Duvidas Frequentes </h2>
+		</figcaption>
+                <button id="duvida1" class="btnDuvida1"><h4>É pago</h4></button><br>
+                <button id="duvida2" class="btnDuvida2"><h4>A comunicacao e confiavel</h4></button><br>
+                <button id="duvida3" class="btnDuvida3"><h4>Como funciona o processo</h4></button><br>
+                <button id="duvida4" class="btnDuvida4"><h4>Quais são os benefícios</h4></button><br>
+                <button id="duvida5" class="btnDuvida5"><h4>Dificuldades em achar oque procura</h4></button>
         </div>
 
             <form class="formulario-contato">
-                <legend>KAJSKAJSKAJKSJ</legend>
-                <div class="nomeContato">
-                    <h5><strong>Nome</strong></h5><input type="text" name="txNomeContato"/>
-                </div>
-                
-                <div class="emailContato">
-                    <h5><strong>E-mail</strong></h5><input type="email" name="txemailContato"/>
-                </div>
-                
-                <div class="mensagemContato">
-                    <h5><strong>Mensagem</strong><h5><br><textarea name="txMensagemContato" maxlenght="200"></textarea>
-                </div>
-                
-                <div class="enviarDuvida">
-                    <input type="submit" placeholder="Enviar" name="enviarDuvida"/>
-                </div>    
+                <legend><strong>Sua duvida ainda persiste? Contate-nos!</strong></legend>
+                    <h5><strong>Nome</strong></h5><input type="text" name="txNomeContato" class="nomeContato"/>
+                    <h5><strong>E-mail</strong></h5><input type="email" name="txemailContato" class="emailContato"/>
+                    <h5><strong>Mensagem</strong><h5><br><textarea name="txMensagemContato" maxlenght="200" class="mensagemContato"></textarea>
+                    <input type="submit" placeholder="Enviar" name="enviarDuvida" class="btnEviarDuvida"/>
             </form>
     </section>
     <footer>
