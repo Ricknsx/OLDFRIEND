62a742cfc023c7460b4ee09ef5ea30ba1fa892fc
danilocucharro <53611163+danilocucharro@users.noreply.github.com>
2020-08-25 11:40:25 -0300

    front-end das  telas de cadastro para cuidador e cliente parcialmente comnpletas

--- view/login-cliente.php
+++ view/login-cliente.php
@@ -30,8 +30,8 @@
 
             <div class="margin-button">
                 <legend class="cadastre-se"><b>CADASTRE-SE</b></legend>
-                <button class="btn-cadastro-cliente"><b>CADASTRAR COMO CLIENTE</b><p>Forneca algumas informacoes, para efetuar<br>buscas por um profissional adequado a suas<br>necessidades</p></button>
-                <button class="btn-cadastro-cuidador"><b>CADASTRAR COMO CUIDADOR</b><p>Forneca algumas informacoes, curriculo e afins<br>para oferecer seus servicos e ser <b>contatado</b><br>por eventuais procuras do seu servico</p></button>
+                <a href="cadastro-cliente.php"><button  class="btn-cadastro-cliente"><b>CADASTRAR COMO CLIENTE</b><p>Forneca algumas informacoes, para efetuar<br>buscas por um profissional adequado a suas<br>necessidades</p></button></a>
+                <a href="cadastro-cuidador.php"><button class="btn-cadastro-cuidador"><b>CADASTRAR COMO CUIDADOR</b><p>Forneca algumas informacoes, curriculo e afins<br>para oferecer seus servicos e ser <b>contatado</b><br>por eventuais procuras do seu servico</p></button></a>
             </div>
         
         </section>
