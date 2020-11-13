2e51632d6170642120b6378c4323de4cadbad680
Ricknsx <ricardon258@gmail.com>
2020-11-13 15:36:28 -0300

    atualização merge

--- controller/conexao.php
+++ controller/conexao.php
@@ -6,11 +6,8 @@
 
 
 	$conn = new mysqli($host, $usuario, $senha, $bd);
-<<<<<<< HEAD
 
 	$conexao= mysqli_connect($host,$usuario,$senha,$bd);
 
 	$connFiltro = new PDO("mysql:host=$host;dbname=$bd",$usuario,$senha); 
-=======
->>>>>>> 51b40073595f9ffa63859ecf5862a732b329ed2b
 ?>
